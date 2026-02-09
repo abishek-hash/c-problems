94f796fa196de5e4ef09f623efde654a069a7a2b
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:17:55 +0530

    Add files via upload

---
+++ Declare two integer variables and print them(t2(4).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+2. Declare two integer variables and print them
+Test Case 1
+ Input: 5 15
+ Output: 5 15
+ 
+Test Case 2
+ Input: 20 40
+ Output: 20 40
+
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int num1,num2,num3,num4;
+   scanf("%d %d\n%d %d",&num1,&num2,&num3,&num4);
+   printf("%d %d\n%d %d",num1,num2,num3,num4);
+    return 0;
+}
