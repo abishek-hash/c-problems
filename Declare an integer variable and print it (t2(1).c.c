8d3044ccaf7dd89c845d1e5ec00394dcd821ee64
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:06:07 +0530

    Add files via upload

---
+++ Declare an integer variable and print it (t2(1).c.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+1. Declare an integer variable and print it
+Test Case 1
+Output: 10
+Test Case 2
+Output: 50
+
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter a two integer:\n");
+    scanf("%d\n%d",&num1,&num2);
+    printf("The 1st integer value is %d\n",num1);
+    printf("The 2nd integer value is %d\n",num2);
+
+    return 0;
+}
