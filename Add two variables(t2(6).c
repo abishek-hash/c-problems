826c6703e673c7db60da8ef690b44d5ba9ab3197
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:26:34 +0530

    Add files via upload

---
+++ Add two variables(t2(6).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+6. Add two variables
+Test Case 1
+ Input: 10 5
+ Output: 15
+Test Case 2
+ Input: 20 10
+ Output: 30
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,Sum;
+    printf("Enter a Two Numbers:\n");
+    scanf("%d\n%d",&num1,&num2);
+    Sum=num1+num2;
+    printf("The sum value is:%d \n",Sum);
+   
+    
+    
+    return 0;
+}
