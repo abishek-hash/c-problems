b4ac7ed831fb55393c0b92e38e3d908941bb1aa1
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 19:30:39 +0530

    Add files via upload

---
+++ print the char variable (t3(4).c.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+4. Print char variable
+Test Case 1
+Output: A
+Test Case 2
+Output: M
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char AB,CD;
+    AB='A';
+    CD='M';
+    printf("%c\n%c",AB,CD);
+
+    return 0;
+}
