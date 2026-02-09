58491e015f12ace4cad95d9bf1642f01a9674db7
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 13:49:10 +0530

    Add files via upload

---
+++ C problems.c.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+2. Print your name
+Test Case 1
+Output: Sivan
+Test Case 2
+Output: Ravi
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char a[10],b[10];
+    scanf("%s\n%s",&a,&b);
+    printf("My Name is:%s\n",a);
+    printf("My Name is:%s",b);
+
+    return 0;
+}
