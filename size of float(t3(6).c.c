d60f4d877d231119a5f8b36bc564af9698a471a7
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 20:10:37 +0530

    Add files via upload

---
+++ size of float(t3(6).c.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+6. Size of float
+Test Case 1
+Output: 4
+Test Case 2
+Output: 4
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   float floattype;
+   printf("size of float:%d bytes",sizeof(floattype));
+
+    return 0;
+}
