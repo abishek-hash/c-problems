187f4dfccefcd938e9107b17645248ffce7ed61a
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 20:18:16 +0530

    Add files via upload

---
+++ size of char(t3(8).c
@@ -0,0 +1,16 @@
+/******************************************************************************
+8. Size of char
+Test Case 1
+Output: 1
+Test Case 2
+Output: 1
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char chartype;
+    printf("size of character is:%d Bytes",sizeof(chartype));
+
+    return 0;
+}
