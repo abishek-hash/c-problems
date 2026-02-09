8f0c3d51f07d25b3efe187ba007fc8bceec02dcf
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:35:12 +0530

    Add files via upload

---
+++ Multiple two values(t2(8).c.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+8. Multiply two variables
+Test Case 1
+Output: 20
+Test Case 2
+Output: 50
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,Mul;
+    printf("Enter a Two Numbers:\n");
+    scanf("%d\n%d",&num1,&num2);
+    Mul=num1*num2;
+    printf("The Mul value is:%d \n",Mul);
+   
+    
+    
+    return 0;
+}
