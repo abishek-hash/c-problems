a080d460006e6eac3b7773a61fc84a1c03ee7032
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 13:59:27 +0530

    Add files via upload

---
+++ integer problem.c.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+3. Print an integer value
+Test Case 1
+Output: 10
+Test Case 2
+Output: 25
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    printf("Enter the integer Value:\n");
+    scanf("%d\n%d",&num1,&num2);
+    printf("The integer value is:%d\n",num1);
+    printf("The integer value is:%d\n",num2);
+
+    return 0;
+}
