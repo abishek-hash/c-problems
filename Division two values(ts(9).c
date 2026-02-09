70db0131fc2884ae391db8ceed4c447539b108ac
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:40:03 +0530

    Add files via upload

---
+++ Division two values(ts(9).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+9. Divide two variables
+Test Case 1
+Output: 4
+Test Case 2
+Output: 5
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,Div;
+    printf("Enter a Two Numbers:\n");
+    scanf("%d\n%d",&num1,&num2);
+    Div=num1/num2;
+    printf("The Div value is:%d \n",Div);
+   
+    
+    
+    return 0;
+}
