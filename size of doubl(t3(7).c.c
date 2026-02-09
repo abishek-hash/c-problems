a173b080e4093e8c73dcb3e890f6206428e5f237
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 20:14:41 +0530

    Add files via upload

---
+++ size of doubl(t3(7).c.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+7. Size of double
+Test Case 1
+Output: 8
+Test Case 2
+Output: 8
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    double doubletype;
+    printf("size of double is:%d Bytes",sizeof(doubletype));
+
+    return 0;
+}
