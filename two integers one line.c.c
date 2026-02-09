59e4a650e8b8181f35a6c2ef069649963dc12e9e
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 14:25:46 +0530

    Add files via upload

---
+++ two integers one line.c.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+6. Print two integers in one line
+Test Case 1
+Output: 5 10
+Test Case 2
+Output: 20 30
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("%d %d",num1,num2);
+
+    return 0;
+}
