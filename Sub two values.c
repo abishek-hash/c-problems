6ae405f82f031eec3e6f90b060899b51cef30239
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-09 15:29:50 +0530

    Add files via upload

---
+++ Sub two values.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+7. Subtract two variables
+Test Case 1
+Output: 5
+Test Case 2
+Output: 10
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,Sub;
+    printf("Enter a Two Numbers:\n");
+    scanf("%d\n%d",&num1,&num2);
+    Sub=num1-num2;
+    printf("The sum value is:%d \n",Sub);
+   
+    
+    
+    return 0;
+}
