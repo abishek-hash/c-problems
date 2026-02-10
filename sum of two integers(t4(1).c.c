7bd580e27bf2a1bb1f532f0b7342d4a1a2420e9a
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-10 11:50:54 +0530

    Add files via upload

---
+++ sum of two integers(t4(1).c.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+1. Print the sum of two integers using +.
+
+Test Case 1
+Input: 5 5
+Output: 10
+Test Case 2
+Input: 3 7
+Output: 10
+Test Case 3
+Input: 8 2
+Output: 10
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int sum,num1,num2;
+    num1=5;
+    num2=5;
+    sum=num1+num2;
+    printf("%d",sum);
+    return 0;
+}
