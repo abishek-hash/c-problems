d2f4c16f30dbb727793c3e6f01cd50d5698588c1
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-11 14:00:17 +0530

    Add files via upload

---
+++ Assign a value and print it(t8(1).c.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+Assign a value and print it.
+Test Case 1
+Input: 10
+Output: 10
+Test Case 2
+Input: 5
+Output: 5
+Test Case 3
+Input: 0
+Output: 0
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
