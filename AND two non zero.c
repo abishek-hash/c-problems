69af5924c2c120ff65c196ca1fdff05453fd1485
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-11 11:05:36 +0530

    Add files via upload

---
+++ AND two non zero.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+ Use logical AND on two non-zero values.
+Test Case 1
+Input: 5 3
+Output: 1
+Test Case 2
+Input: 1 1
+Output: 1
+Test Case 3
+Input: 0 5
+Output: 0
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    printf("%d",num1&&num2);
+
+    return 0;
+}
