c76666204d29ad542edde6efd110df2db50d57ee
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-11 11:26:40 +0530

    Add files via upload

---
+++ logical not operator(t7(3).c.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+Apply logical NOT on zero.
+Test Case 1
+Input: 0
+Output: 1
+Test Case 2
+Input: 0
+Output: 1
+Test Case 3
+Input: 0
+Output: 1
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d",!a);
+
+    return 0;
+}
