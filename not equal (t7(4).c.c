70356e2f2908787b71333e6cc19bdc995a16e0e1
abishek-hash <abishekpadmanaban010@gmail.com>
2026-02-11 11:42:23 +0530

    Add files via upload

---
+++ not equal (t7(4).c.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+Apply logical NOT on non-zero value.
+Test Case 1
+Input: 5
+Output: 0
+Test Case 2
+Input: 1
+Output: 0
+Test Case 3
+Input: -3
+Output: 0
+*******************************************************************************/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d",!a);
+    return 0;
+}
